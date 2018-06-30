4191477e93cd66f6bc8fade6a8a940e30a38f5e6
matthewhilden <38081791+matthewhilden@users.noreply.github.com>
2018-06-29 19:10:17 -0700

    Added a simple unit test that ensures that GPAs must be less than or equal to 4.0.

--- student/src/test/java/edu/pdx/cs410J/hilden/StudentTest.java
+++ student/src/test/java/edu/pdx/cs410J/hilden/StudentTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static junit.framework.TestCase.fail;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -12,14 +13,27 @@ import static org.hamcrest.core.IsEqual.equalTo;
  * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
  * matchers for more readable assertion statements.
  */
-public class StudentTest
-{
+public class StudentTest {
 
-  @Test
-  public void studentNamedPatIsNamedPat() {
-    String name = "Pat";
-    var pat = new Student(name, new ArrayList<>(), 0.0, "Doesn't matter");
-    assertThat(pat.getName(), equalTo(name));
-  }
+    @Test
+    public void studentNamedPatIsNamedPat() {
+        String name = "Pat";
+        var pat = new Student(name, new ArrayList<>(), 0.0, "Doesn't matter");
+        assertThat(pat.getName(), equalTo(name));
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void gpaMustBeLessThanOrEqualToFourPointZero() {
+        createStudentWithGpa(4.1);
+    }
+
+    @Test
+    public void aFourPointZeroGpaIsPossible()
+    {
+        createStudentWithGpa(4.0);
+    }
+
+    private void createStudentWithGpa(double gpa) {
+        new Student("??", new ArrayList<>(), gpa, "Doesn't matter");
+    }
 }
